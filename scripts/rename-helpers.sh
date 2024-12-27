1daa4005e36db67895f201f7a7af99b194a9ee44
Jason Lieb <Jason.lieb@outlook.com>
2024-12-27 01:25:41 -0500

    exit rename helpers script if there isnt a _commonjsHelpers file

--- scripts/rename-helpers.sh
+++ scripts/rename-helpers.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 filename=$(ls dist | grep -E '^_commonjsHelpers\.[^.]+\.js$' | awk '{print $1}')
+[ -z "$filename" ] && exit 0
 new_filename=$(echo "$filename" | sed 's/^_//')
 mv "dist/$filename" "dist/$new_filename"
 sed -i "s|$filename|$new_filename|g" dist/src/main.js
